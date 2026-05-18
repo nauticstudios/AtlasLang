045752219e9eb7b6524ad114f58da4537e12f1c4
senkex <ssenkex@gmail.com>
2026-05-18 00:22:56 -0300

    refactor(core): remove deprecated components and configurations

--- src/main/java/com/github/nautic/database/DatabaseType.java
+++
@@ -1,6 +0,0 @@
-package com.github.nautic.database;
-
-public enum DatabaseType {
-    H2,
-    MYSQL
-}
--- src/main/java/com/github/nautic/github/GitHubSyncResult.java
+++
@@ -1,7 +0,0 @@
-package com.github.nautic.github;
-
-public enum GitHubSyncResult {
-    SUCCESS,
-    NO_CHANGES,
-    FAILED
-}
